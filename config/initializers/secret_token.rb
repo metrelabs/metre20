7432756644542034cf73b1db89df72ef77a2d991
mostlyclicks <carlos@mostlyclicks.com>
2013-06-10 10:52:51 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metre20::Application.config.secret_token = '3ae87e9fbb16e52cb25f263fdc73adde29f516a9c1159bd66865afcf222561c51bac3b9fd993e7531c435b877961e07315df522e579ac447c3df6c0195d7516f'
