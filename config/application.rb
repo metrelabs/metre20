ea2a4059f20f0117715c86603da1e57c56d2ca6c
mostlyclicks <carlos@mostlyclicks.com>
2013-06-11 13:41:27 -0500

    added comment to app.rb

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,7 @@ module Metre20
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    #for AWS
     config.assets.initialize_on_precompile = true
     
   end
