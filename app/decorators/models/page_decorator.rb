2a6410529c6d585e233f4df54c003f2022f62209
mostlyclicks <carlos@mostlyclicks.com>
2013-06-18 15:06:42 -0500

    add white text switch

--- app/decorators/models/page_decorator.rb
+++ app/decorators/models/page_decorator.rb
@@ -1,5 +1,7 @@
 Refinery::Page.class_eval do
-	attr_accessible :background_image_id
+	attr_accessible :background_image_id, :white_text
+	
 
 	belongs_to :background_image, :class_name => '::Refinery::Image'
+	
 end
