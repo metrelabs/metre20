6f05c994e0b4c3ea361d60486483a9d4d7927344
mostlyclicks <carlos@mostlyclicks.com>
2013-07-09 11:14:39 -0500

    assets precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,9 @@ Metre20::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+  # Heroku REQUIRES this to be false
+  config.assets.initialize_on_precompile = true
+
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
