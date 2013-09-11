9eaac956c5b17b782148d9e64b1c2fc8cd896347
mostlyclicks <carlos@mostlyclicks.com>
2013-09-11 09:23:43 -0500

    pull last 5 tweets instead of all

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   require 'twitter-text'
-	include Twitter::Autolink
+  	include Twitter::Autolink
+
+  before_filter :get_tweets
+
+  
+  private
+
+  def get_tweets
+  	tweets = TweetFav.all
+  	@recent_tweets = tweets.last(5)
+  end
 
 	
 
